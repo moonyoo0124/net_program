42ba1306e816f3e4aac90c4fca33d1b247230f1b
mooonyoo <moony240224@gmail.com>
2025-04-01 20:11:43 +0900

    프로젝트 파일 추가

---
+++ name.py
@@ -0,0 +1,31 @@
+my_string = "My name is moonyoo"
+
+# 1. 문자열의 문자 수 출력
+print(len(my_string))
+
+# 2. 문자열을 10번 반복한 문자열 출력
+print(my_string * 10)
+
+# 3. 문자열의 첫 번째 문자 출력
+print(my_string[0])
+
+# 4. 문자열에서 처음 4문자 출력
+print(my_string[:4])
+
+# 5. 문자열에서 마지막 4문자 출력
+print(my_string[-4:])
+
+# 6. 문자열의 문자를 거꾸로 출력
+print(my_string[::-1])
+
+# 7. 문자열에서 첫 번째 문자와 마지막 문자를 제거한 문자열 출력
+print(my_string[1:-1])
+
+# 8. 문자를 모두 대문자로 변경하여 출력
+print(my_string.upper())
+
+# 9. 문자를 모두 소문자로 변경하여 출력
+print(my_string.lower())
+
+# 10. 문자열에서 'a'를 'e'로 대체하여 출력
+print(my_string.replace('a', 'e'))
